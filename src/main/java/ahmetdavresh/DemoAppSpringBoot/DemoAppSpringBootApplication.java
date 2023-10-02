5c4e619084e7facd596f721727a6306fc8c1ff9c
ahmet <ahmetdavresh@gmail.com>
2023-10-02 22:16:59 +0300

    init

---
+++ src/main/java/ahmetdavresh/DemoAppSpringBoot/DemoAppSpringBootApplication.java
@@ -0,0 +1,11 @@
+package ahmetdavresh.DemoAppSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppSpringBootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppSpringBootApplication.class, args);
+	}
+}
---
+++ src/test/java/ahmetdavresh/DemoAppSpringBoot/DemoAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ahmetdavresh.DemoAppSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
